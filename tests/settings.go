0355dc8324d2fa75ac07d805d383a26a55558c74
Клим Васильев <vasilyevklim@yandex.ru>
2025-07-25 20:03:27 +0300

    token update

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTM0MzYwNzksImhhc2giOiI1OTk0NDcxYWJiMDExMTJhZmNjMTgxNTlmNmNjNzRiNGY1MTFiOTk4MDZkYTU5YjNjYWY1YTljMTczY2FjZmM1In0.ZTCZtKtNLoCz3_DUAMjvt_x3ICqPgHRAtTZA2Fqd5Js`
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTM0OTE3MjAsImhhc2giOiI1OTk0NDcxYWJiMDExMTJhZmNjMTgxNTlmNmNjNzRiNGY1MTFiOTk4MDZkYTU5YjNjYWY1YTljMTczY2FjZmM1In0.tqDgWgMN_LB5vlaby-r8lxiBYXd-E-Wimc09wrrjHjI`
